1a811cb4f2f354c0d1a1e3fac752f81d240d1942
Anthony Le Courric <freelance.anthony@gmail.com>
2019-11-18 17:21:26 +0100

    hamburger close menu

--- frontend/src/components/MenuLeft/MenuLeft.component.jsx
+++ frontend/src/components/MenuLeft/MenuLeft.component.jsx
@@ -1,50 +1,51 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './MenuLeft.scss';
 import Logo from './logo.svg';
 import { NavLink } from 'react-router-dom';
 import {connect} from 'react-redux';
 import {toggleMenuLeft} from '../../redux/class/class.action';
 
-class MenuLeft extends React.Component{
-  links = [
+const MenuLeft = ({toggleMenuLeft}) => {
+  const links = [
     {id: 1, path: '/', name: 'Dashboard', class: 'fas fa-chart-line'},
     {id: 2, path: '/trade', name: 'Trade', class: 'far fa-edit'},
   ];
-  
-  componentDidMount(){
-    const {toggleMenuLeft}= this.props;
+  const [isActive, setIsActive] = useState(true);
+
+  useEffect(() => {
     setTimeout(()=>(toggleMenuLeft()), 700);
-  }
+  },[]);
   
-  render(){
-    const {toggleMenuLeft}= this.props;
-    return(
-      <div className='menu-left-container'>
-        <div className="menu-left-header">
-          <img src={Logo}/>
-          <span className="title">inance Bot</span>
-        </div>
-        <div className="nav-block">
-        {
-          this.links.map((link)=>(
-            <NavLink 
-              exact
-              key={link.id} 
-              to={link.path} 
-              className='menu-left-link'
-              activeClassName="selected-link">
-              <i className={`${link.class} menu-icon`}></i>
-              {link.name}
-            </NavLink>
-            ))
-        }
-        </div>
-        <div className="hide-show-menu" onClick={()=>toggleMenuLeft()}>
-          <i className="fas fa-sign-in-alt"></i>
+  return(
+    <div className='menu-left-container'>
+      <div className="menu-left-header">
+        <img src={Logo}/>
+        <span className="title">inance Bot</span>
+      </div>
+      <div className="nav-block">
+      {
+        links.map((link)=>(
+          <NavLink 
+            exact
+            key={link.id} 
+            to={link.path} 
+            className='menu-left-link'
+            activeClassName="selected-link">
+            <i className={`${link.class} menu-icon`}></i>
+            {link.name}
+          </NavLink>
+          ))
+      }
+      </div>
+      <div className="hide-show-menu" onClick={()=>{setIsActive(!isActive);toggleMenuLeft()}}>
+        <div className={"hamburger hamburger--spring js-hamburger " + ((isActive) ? "is-active": "")}>
+          <div className="hamburger-box">
+            <div className="hamburger-inner"></div>
+          </div>
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }
     
 const mapDispatchToProps = dispatch => ({
