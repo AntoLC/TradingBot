16305b6eb067cfdab104bb20e3e5fd900d2f0aba
Anthony Le Courric <freelance.anthony@gmail.com>
2019-11-20 11:08:17 +0100

    heroku http socket

--- frontend/src/redux/dataSocket/dataSocket.conf.js
+++ frontend/src/redux/dataSocket/dataSocket.conf.js
@@ -1,7 +1,7 @@
 
 console.debug(process.env.REACT_APP_PORT_SERVER);
 const dataSocketConf = {
-    ENDPOINT: "localhost:"+process.env.REACT_APP_PORT_SERVER+"/",
+    ENDPOINT: "http://localhost:"+process.env.REACT_APP_PORT_SERVER+"/",
     ROOM: "BinanceIO",
     REQUEST: "request",
     INIT_SYMBOL: "BTCUSDT",
