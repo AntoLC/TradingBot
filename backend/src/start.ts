93134c34902965bf8d0b1b5c26ee50411a61fc31
Anthony Le Courric <freelance.anthony@gmail.com>
2019-11-20 13:56:36 +0100

    debug backend

--- backend/src/start.ts
+++ backend/src/start.ts
@@ -4,8 +4,8 @@ import { logger } from '@shared';
 
 // Start the server
 const port = Number(process.env.PORT || 3000);
-app.listen(port, () => {
+/*app.listen(port, () => {
     logger.info('Express server started on port: ' + port);
-});
+});*/
 
 SocketInit();
